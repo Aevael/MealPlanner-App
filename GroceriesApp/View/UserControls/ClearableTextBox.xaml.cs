5ec3c9b04ee3c1384db716032b5c4a7d20d22df5
Aevael <98851230+Aevael@users.noreply.github.com>
2025-07-09 21:40:20 -0500

    Clearable Text Box User Control Implementation

---
+++ GroceriesApp/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroceriesApp.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ClearableTextBox.xaml
+    /// </summary>
+    public partial class ClearableTextBox : UserControl
+    {
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private string placeHolder;
+
+        public string PlaceHolder
+        {
+            get { return placeHolder; }
+            set 
+            { 
+                placeHolder = value;
+                placeHolderText.Text = placeHolder;
+            }
+        }
+
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            textInput.Clear();
+            textInput.Focus();
+
+        }
+
+        private void TextInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(textInput.Text)){
+                placeHolderText.Visibility = Visibility.Visible;   
+            }
+            else
+            {
+                placeHolderText.Visibility = Visibility.Hidden;    
+            }
+
+
+
+        }
+    }
+}
