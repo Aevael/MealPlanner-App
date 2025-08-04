845b72e81e76983bdb7a539c154ac7bf522e917e
Aevael <98851230+Aevael@users.noreply.github.com>
2025-08-03 21:47:19 -0500

    Added Condiment Class.

---
+++ MealPlannerApp/Model/Condiment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPlannerApp.Model
+{
+    internal class Condiment
+    {
+        string Name;
+        Boolean InStock;
+
+    }
+}
