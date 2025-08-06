cad80c7c0a292919ae2f4ae530e7442d5c0a445a
Aevael <98851230+Aevael@users.noreply.github.com>
2025-08-06 15:28:32 -0500

    Added Ingredient Class.

---
+++ MealPlannerApp/Model/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPlannerApp.Model
+{
+    internal class Ingredient
+    {
+        string Name { get; set; }
+        string Quantity { get; set; }
+    }
+}
